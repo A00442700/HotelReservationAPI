26a54e93ef4ebc3dc3e28d45dfcab5fb719c01dd
Nikitha <nikitha.medisetty@smu.ca>
2021-07-26 14:50:58 -0700

    returning model

--- src/main/java/com/hotelreservation/controller/HotelController.java
+++ src/main/java/com/hotelreservation/controller/HotelController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hotelreservation.model.ConfirmationNumber;
 import com.hotelreservation.model.HotelDetails;
 import com.hotelreservation.model.ReservationDetails;
 
@@ -23,9 +24,19 @@ public class HotelController {
 	}
 	
 	@RequestMapping(value = "/reservationConfirmation", method=RequestMethod.POST,consumes="application/json")
-	public String reserveHotel(@RequestBody ReservationDetails reservationDetails)
+	public ConfirmationNumber reserveHotel(@RequestBody ReservationDetails reservationDetails)
 	{
-		return reservationDetails.getHotel_name();
+		Random rand = new Random(); //instance of random class
+	    int upperbound = 125;
+	        //generate random values from 0-24
+	    int int_random = rand.nextInt(upperbound); 
+	    String reservationNumber = String.valueOf(int_random);
+	    ConfirmationNumber number = new ConfirmationNumber();
+	    
+	    number.setReservationNumber(reservationNumber);
+	   
+	    
+		return number;
 		
 	}
 	
@@ -34,18 +45,32 @@ public class HotelController {
 		List<HotelDetails> hotelsList = new ArrayList<HotelDetails>();
 		try 
 		{
-			
+			/*
+			HotelDetails hotelDetails1 = new HotelDetails("Holiday Inn",100,true);
+			HotelDetails hotelDetails2 = new HotelDetails("Marriot",100,true);
+			HotelDetails hotelDetails3 = new HotelDetails("Comfort Inn",100,true);
+			HotelDetails hotelDetails4 = new HotelDetails("Crowne Plaza",100,true);
+			*/
 			List<String> hotelNames = Arrays.asList("Holiday Inn","Marriot","Comfort Inn","Crowne Plaza");
 			Random random = new Random();
 			for(int i=0; i<hotelNames.size();i++)
 			{
-				HotelDetails hotelDetails = new HotelDetails();
+				HotelDetails hotelDetails = new HotelDetails("Holiday Inn",100,true);
+				
 				hotelDetails.setHotel_name(hotelNames.get(i));
 				hotelDetails.setAvailability(true);
 				int randomPrice = random.nextInt(200 + 50 +1) +50 ;
 				hotelDetails.setPrice(randomPrice);
-				hotelsList.add(hotelDetails);			
-			}			
+				hotelsList.add(hotelDetails);
+				
+			}
+			
+			/*
+			hotelsList.add(hotelDetails3);
+			hotelsList.add(hotelDetails1);
+			hotelsList.add(hotelDetails2);
+			hotelsList.add(hotelDetails4);
+			*/
 		}
 		catch(Exception ex)
 		{
---
+++ src/main/java/com/hotelreservation/model/ConfirmationNumber.java
@@ -0,0 +1,16 @@
+package com.hotelreservation.model;
+
+public class ConfirmationNumber {
+	
+	public String reservationNumber;
+
+	public String getReservationNumber() {
+		return reservationNumber;
+	}
+
+	public void setReservationNumber(String reservationNumber) {
+		this.reservationNumber = reservationNumber;
+	}
+	
+
+}
